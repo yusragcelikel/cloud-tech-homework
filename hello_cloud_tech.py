679f445e14d9e72be696c322d7b8ad4ed7b466b9
Yusra Celikel <yusragcelikel@gmail.com>
2024-01-12 19:59:25 +0300

    made an update to scan the code

--- hello_cloud_tech.py
+++ hello_cloud_tech.py
@@ -1,1 +1,4 @@
-print("Hello cloud tech class!!")
+introduction = "Hello cloud tech class!!"
+print(introduction)
+user_input = input("Introduce yourself to the class. What's your name?")
+print(introduction, "My name is", user_input + ".")
